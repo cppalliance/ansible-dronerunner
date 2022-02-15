27f395faddbc5140eaa13976792e5bee06901144
sdarwin <samuel.d.darwin@gmail.com>
2022-02-15 23:38:30 +0000

    Rearranging steps

--- scripts/bootstrap_win.ps1
+++ scripts/bootstrap_win.ps1
@@ -8,11 +8,10 @@ $keysfile = "C:\ProgramData\ssh\administrators_authorized_keys"
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y --package-parameters=/SSHServerFeature openssh
 
-Add-Content C:\ProgramData\ssh\sshd_config "PasswordAuthentication no"
-Restart-Service -Name sshd
-
 Add-Content $keysfile $pubkey1
 icacls $keysfile /inheritance:d
 icacls $keysfile /remove "Users"
 icacls $keysfile /remove "Authenticated Users"
 
+Add-Content C:\ProgramData\ssh\sshd_config "PasswordAuthentication no"
+Restart-Service -Name sshd
