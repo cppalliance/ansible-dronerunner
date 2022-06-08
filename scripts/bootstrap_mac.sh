da567e8827e431f55c8450d5f387ec944d063f40
sdarwin <samuel.d.darwin@gmail.com>
2022-06-08 17:42:33 -0600

    OSX 12.4 updates

--- scripts/bootstrap_mac.sh
+++ scripts/bootstrap_mac.sh
@@ -27,14 +27,6 @@ group=staff
 sshdir=/Users/administrator/.ssh
 pubkey1="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCH0oawPzIylSjdu/fpyDD2i2stkqe52bFmLT8+MeiTAp5WI8BwlbeeiiZkneEHhLW7bGMKZ50rQONjiudWCFibb4zM2pUQTFP91BuzUG7MjFf179UlvRMUiNSYkKSSB4q0QZ8+2Vjj5lXzYxM5FjZ9FdA1ioI5l8TK8rLlf/F1TKKDfjA/YMk7769BVYndDilSidaDEvRVxQM8Z5RBUnSnDFQwEaVOuVaHIki0ZPVecwyE96e2HaFDRjNlMUZbSgHrdwkjbIugaUfiWFANBA5eIOka19CSLV5aY1tNeawoUvIBsRXjUleFJE+EIL0iGcuTcLXvAqh5UwFdMkkwUfhH drone-runner"
 
-if [ -z "$FASTLANE_USER" ] || [ -z "FASTLANE_PASSWORD" ]; then
-  echo "Set both FASTLANE_USER and FASTLANE_PASSWORD:
-export FASTLANE_USER=
-export FASTLANE_PASSWORD=
-"
-exit 1
-fi
-
 if [ ! -f /etc/sudoers.d/$user ]; then
     sudo echo "$user ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$user
     echo "You may need to logout and login again before continuing."
@@ -45,6 +37,14 @@ mkdir -p $sshdir
 echo "$pubkey1" > $sshdir/authorized_keys
 chmod -R 700 $sshdir
 chown -R $user:$group $sshdir
+
+if [ -z "$FASTLANE_USER" ] || [ -z "FASTLANE_PASSWORD" ]; then
+  echo "Set both FASTLANE_USER and FASTLANE_PASSWORD:
+export FASTLANE_USER=
+export FASTLANE_PASSWORD=
+"
+exit 1
+fi
 
 # ##########################################################################
 
@@ -66,7 +66,44 @@ brew install pkg-config
 brew install openssl
 
 opensslpackage=$(brew list | grep openssl | tail -n 1)
-ln -s /usr/local/opt/$opensslpackage /usr/local/opt/openssl
+ln -s /usr/local/opt/$opensslpackage /usr/local/opt/openssl || true
+
+if [[ "$(sw_vers -productVersion)" =~ "12.4" ]] ; then
+    xcodeversions="12.5 12.5.1 13.0 13.1 13.2 13.2.1 13.3 13.3.1 13.4 13.4.1"
+    sudo xcrun gem install xcode-install --no-document
+    # 2022-03-07 latest experiment, will this happen automatically if needed?
+    # That is, will it prompt you for a login and 2FA.
+    # if [ ! -d /Users/administrator/.fastlane/spaceship ]; then
+    #     echo "Configuring auth"
+    #     read -p "Apple user name:" appleuser
+    #     fastlane spaceauth -u $appleuser
+    #     read -p "Do you wish to continue?" yn
+    # fi
+    for xcodeversion in $xcodeversions; do
+        if [ ! -d /Applications/Xcode-$xcodeversion.app ]; then
+            xcversion install $xcodeversion --no-switch
+        else
+            echo "Directory /Applications/Xcode-$xcodeversion.app already exists."
+        fi
+        # and then check the result
+        if [ ! -d /Applications/Xcode-$xcodeversion.app ]; then
+            echo "Failed to install /Applications/Xcode-$xcodeversion.app"
+            exit 1
+        fi
+    done
+
+    cd /Applications
+
+    if [ -d /Library/Developer/CommandLineTools ]; then
+        sudo mv /Library/Developer/CommandLineTools /Library/Developer/CommandLineTools.bck
+    fi
+    sudo xcode-select -switch /Applications/Xcode-12.5.app/Contents/Developer
+    sudo xcodebuild -license accept
+    sudo xcode-select -switch /Applications/Xcode-13.4.1.app/Contents/Developer
+    sudo xcodebuild -license accept
+
+    brew install bash
+fi
 
 if [[ "$(sw_vers -productVersion)" =~ "10.15" ]] ; then
     xcodeversions="10 10.1 10.2 10.3 11 11.1 11.2 11.2.1 11.3 11.4 11.5 11.6 11.7 12 12.1 12.2 12.3 12.4"
